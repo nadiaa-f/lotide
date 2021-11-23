4fb7c9d7d4b887502014e0fec615721907834906
Nadia Fatah <nadi.fatah@gmail.com>
2021-11-23 06:11:39 +0000

    lint corrected

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Asseration Passed: ${actual} === ${expected}`);
   } else {
-      console.log(`🛑🛑🛑Asseration Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑Asseration Failed: ${actual} !== ${expected}`);
   }
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
--- head.js
+++ head.js
@@ -2,14 +2,14 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Asseration Passed: ${actual} === ${expected}`);
   } else {
-      console.log(`🛑🛑🛑Asseration Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑Asseration Failed: ${actual} !== ${expected}`);
   }
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
-let head = function (firstElement) {
+let head = function(firstElement) {
   return firstElement[0];
-}
+};
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
