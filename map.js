a7f6d657fcae8d4cc24fb2c7a72a18129edfde7a
Nadia Fatah <nadi.fatah@gmail.com>
2021-11-26 19:29:43 +0000

    implement map

---
+++ map.js
@@ -0,0 +1,31 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return "false";
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo))
+    return console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  else return console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual(results1, ["t", "o", "m", "f", "u"]);
