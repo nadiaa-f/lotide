76e04a7a5129d7ae11adeb3d2b756fa93c03a757
Nadia Fatah <nadi.fatah@gmail.com>
2021-11-26 20:10:11 +0000

    implement takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,41 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return "false";
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo))
+    return console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  else return console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+};
+
+const takeUntil = function(array, callback) {
+  let stored = [];
+
+  for (let word of array) {
+    if (!callback(word))
+      stored.push(word);
+    else
+      break;
+  }
+  return stored;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [1,2,5,7,2]); 
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]); 
