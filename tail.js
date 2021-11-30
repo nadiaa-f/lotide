f5fae88456c5b4d9e73b504c673a0d6d36d2f9f0
Nadia Fatah <nadi.fatah@gmail.com>
2021-11-30 05:52:57 +0000

    refactored code

--- tail.js
+++ tail.js
@@ -1,16 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Asseration Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Asseration Failed: ${actual} !== ${expected}`);
-  }
-};
-
+/*returns the "tail" of an array: everything except for the first item (head) of the provided array*/
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
 };
-
+/*tests moved to tailTest.js
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
@@ -21,4 +14,5 @@ assertEqual(words1.length, 1);
 
 const words0 = [];
 tail(words0);
-assertEqual(words0.length, 0);
+assertEqual(words0.length, 0);*/
+module.exports = tail;
