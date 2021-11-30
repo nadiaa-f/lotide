9a233d054cac9bc65affc8e610275fdd0dad411d
Nadia Fatah <nadi.fatah@gmail.com>
2021-11-30 12:49:31 +0000

    exporting functions

---
+++ index.js
@@ -0,0 +1,16 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual')
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertEqual,
+  assertArraysEqual,
+  eqArrays
+};
