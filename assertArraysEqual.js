5e40b69cc9be1df0b94087169d08dae8e2537047
Nadia Fatah <nadi.fatah@gmail.com>
2021-11-30 10:51:37 +0000

    refactored code (deletion fix)

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,7 @@
-const eqArrays = function(array1, array2) {
+const eqArrays = require('./eqArrays');
+/*copied from eqArrays.js
+takes in two arrays and returns true or false, based on a perfect match.*/
+/*const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return "false";
   }
@@ -9,11 +12,15 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-
+This function is now being called using require*/
+/*takes in two arrays and console.log an appropriate message to the console.*/
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   if (eqArrays(arrayOne, arrayTwo))
     return console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
   else return console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
 };
+
+/*tests moved to assertArraysEqualTest.js
 assertArraysEqual([5, 6, 7, 8], [5, 6, 7, 8]);
-assertArraysEqual([5, 6, 3, 8], [5, 6, 1, 8]);
+assertArraysEqual([5, 6, 3, 8], [5, 6, 1, 8]);*/
+module.exports = assertArraysEqual;
