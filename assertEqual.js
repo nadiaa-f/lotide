d020b721db40e8580e471fd38357703b7a73d2cf
Nadia Fatah <nadi.fatah@gmail.com>
2021-11-30 05:43:26 +0000

    refactored code

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+/*compares the two values it takes in and print out a message telling us if they match or not*/
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Asseration Passed: ${actual} === ${expected}`);
@@ -5,5 +6,9 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑Asseration Failed: ${actual} !== ${expected}`);
   }
 };
+/*tests moved to assertEqualtest.js
+
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual(1, 1);*/
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,15 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Asseration Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Asseration Failed: ${actual} !== ${expected}`);
-  }
-};
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
+//returns the first item in the array
 let head = function(firstElement) {
   return firstElement[0];
 };
+/*tests moved to headTest.js
+
 assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual([""]);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");*/
+
+module.exports = head;
