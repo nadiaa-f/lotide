cedbeb3394a14ac310e111d813d892fcf3370dcc
Nadia Fatah <nadi.fatah@gmail.com>
2021-11-30 11:44:32 +0000

    refactoring to use Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,18 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
+/*const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 //tests
 assertEqual(head([5,6,7]), 5);
 assertEqual([""]);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");*/
