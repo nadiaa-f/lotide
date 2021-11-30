9290ec26d67267910fbb49f39a519ebe24e4ccaf
Nadia Fatah <nadi.fatah@gmail.com>
2021-11-30 07:26:13 +0000

    added line 1 assertEqual require to fix assertArraysEqualTest TypeError

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,4 @@
+const assertEqual = require('./assertEqual');
 /*takes in two arrays and returns true or false, based on a perfect match.*/
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
